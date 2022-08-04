edd48361388971976e8dfa85acf16d5d2721c849
lucassamuelDEV <lucassamuel.dev@gmail.com>
2022-08-04 00:38:36 +0000

    Update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,16 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      white: '#F9F9F9',
-      white200: '#EBE9EA',
-      gray: '#474747',
-      blackLight: '#181818',
+      colors: {
+        white: {
+          DEFAULT: '#F9F9F9',
+          200: '#EBE9EA',
+        },
+        gray: '#474747',
+        black: {
+          ligth: '#181818',
+        },
+      },
     },
     fontFamily: {
       sans: "'Montserrat', sans-serif",
